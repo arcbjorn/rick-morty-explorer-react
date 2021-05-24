ad03806552ef42078203c94f50085e176872edfc
arcbjorn <arky.happ@gmail.com>
2021-05-24 18:20:50 +0300

    feat (store): update RootStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,20 +2,23 @@
  * Import all your stores
  */
 import { CharacterStore } from './character.store';
- import { LocationStore } from './location.store';
- import { EpisodeStore } from './episode.store';
+import { LocationStore } from './location.store';
+import { EpisodeStore } from './episode.store';
  
- /**
-  * Root Store Class
-  */
- export class RootStore {
-   characterStore: CharacterStore;
-   locationStore: LocationStore;
-   episodeStore: EpisodeStore;
- 
-   constructor() {
-     this.characterStore = new CharacterStore();
-     this.locationStore = new LocationStore();
-     this.episodeStore = new EpisodeStore(); // easy communication with `this`
-   }
- }
+/**
+* Root Store Class
+*/
+export class RootStore {
+  characterStore: CharacterStore;
+  locationStore: LocationStore;
+  episodeStore: EpisodeStore;
+
+  constructor() {
+    this.characterStore = new CharacterStore();
+    this.locationStore = new LocationStore();
+    this.episodeStore = new EpisodeStore(); // easy communication with `this`
+  }
+}
+
+const store = new RootStore();
+export default store;
